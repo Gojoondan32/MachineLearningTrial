d44f127ae6eb49bc0a91dcf054061a1d509e6e2d
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2022-11-04 20:31:05 +0000

    Added base mlagents

---
+++ MachineLearningProject/Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetPosition;
+    [SerializeField] private Material winMat;
+    [SerializeField] private Material loseMat;
+    [SerializeField] private MeshRenderer groundMeshRend;
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetPosition.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float x = actions.ContinuousActions[0];
+        float z = actions.ContinuousActions[1];
+
+        float moveSpeed = 4f;
+        transform.localPosition += new Vector3(x, 0, z) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other){
+        //Debug.Log("Running");
+        if(other.gameObject.tag == "Goal"){
+            SetReward(1f);
+            groundMeshRend.material = winMat;
+            EndEpisode();
+        }
+        if(other.gameObject.tag == "Wall"){
+            SetReward(-1f);
+            groundMeshRend.material = loseMat;
+            EndEpisode();
+        }
+    }
+}
