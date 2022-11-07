4ef7ad306cbe60d85c8d174f3462a44945292a31
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2022-11-07 18:06:50 +0000

    Finished Car controller

--- MachineLearningProject/Assets/Scripts/RacingAgent/TestMoveScript.cs
+++ MachineLearningProject/Assets/Scripts/RacingAgent/TestMoveScript.cs
@@ -2,56 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TestMoveScript : MonoBehaviour
 {
-    private Rigidbody rb;
-    [SerializeField] private float inputX;
-    private float inputZ;
-    private float angle;
-    [SerializeField] float rotatationSpeed;
-    [SerializeField] private float moveSpeed;    
-
-    //Angle = 30
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotatationSpeed;
 
 
+    [Header("Test")]
+    [SerializeField] private Transform obj;
 
-    private void Awake() {
-        rb = GetComponent<Rigidbody>();
-        Debug.DrawRay(transform.position, CalcuateVectorRotation(), Color.green, 100f);
-        //transform.forward = CalcuateVectorRotation();
-    }
-    
-    
     private void Update() {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputZ = Input.GetAxisRaw("Vertical");
-
+        if(Input.GetAxisRaw("Horizontal") != 0){
+            Vector3 pointToTurnTo = (transform.forward + transform.right) * Input.GetAxisRaw("Horizontal");
+            obj.position = transform.position + pointToTurnTo; //Debugging
+            Vector3 newDir = ((pointToTurnTo + transform.position) - transform.position).normalized;
+            //Debug.DrawRay(transform.position, newDir, Color.green, 100f);
+            transform.forward = Vector3.Lerp(transform.forward, newDir, rotatationSpeed * Time.deltaTime);
+        }
         
-        angle += inputX * Time.deltaTime * rotatationSpeed;
-        transform.localEulerAngles = new Vector3(0, angle, 0);
-        
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(90 * inputX, Vector3.up), rotatationSpeed * Time.deltaTime);
-
-        //transform.rotation = Quaternion.AngleAxis(90, Vector3.up); //Always rotates it to y 90 
-        //Vector3 newDir = new Vector3(inputX, 0, 0) - transform.forward;
-        //transform.forward = Vector3.Lerp(transform.forward, newDir, Time.deltaTime * rotatationSpeed);
     }
 
     private void FixedUpdate() {
-        Vector3 force = Vector3.ClampMagnitude(transform.forward * moveSpeed, 15);
-        rb.AddForce(force, ForceMode.Acceleration);
-        //rb.velocity = new Vector3(inputX, 0, inputZ) * moveSpeed * Time.deltaTime;
-    }
-    
-
-    private Vector3 CalcuateVectorRotation(){
-        float angle = 30f;
-        Vector3 origDir = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
-        Vector3 perpendicularDir = new Vector3(-Mathf.Sin(angle), Mathf.Cos(angle)).normalized;
-
-        Vector3 forward = transform.forward;
-
-        Vector3 result = (forward.x * origDir) + (forward.z * perpendicularDir);
-        return (result - transform.position).normalized;
+        rb.velocity = transform.forward * speed * Time.deltaTime;
     }
 }
